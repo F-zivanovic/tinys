df8dc5f5872eb503f5e828e63dc9c2025abe0aa5
F-zivanovic <filip.zivanovic1999@gmail.com>
2025-01-03 13:56:05 +0100

    Counting function

--- src/js/main.js
+++ src/js/main.js
@@ -88,6 +88,41 @@ faqItems.forEach(item=>{
 });
 
 
+// Counter
+try {
+    
+    const numbers = document.querySelectorAll('[data-key="number"]');
+let countingStarted  = false;
+
+function counting(number) {
+    let target = parseInt(number.getAttribute('data-target'));
+    let count = 0;
+    let speed = 20;
+
+    let counting = setInterval(()=>{
+        if(count <= target){
+            count += Math.ceil(target / 100);;
+            number.innerHTML = count;
+        }else{
+            number.innerHTML = target;
+            clearInterval(counting);
+        }
+    }, speed)
+}
+
+window.addEventListener('scroll', ()=>{
+    let sectionPosition = numbers[0].closest('section').getBoundingClientRect().top*1.3;
+    let screenHeight = window.innerHeight;
+
+    if(sectionPosition < screenHeight && !countingStarted ){
+        numbers.forEach(number => counting(number));
+        countingStarted  = true;
+    }
+});
+    
+} catch (e) {
+    console.log(e);
+}
 
 
  
